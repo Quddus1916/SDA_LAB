d1759268ac68ff22118ce9a564ebd59c72d6661e
Quddus1916 <quddusjunior1916@gmail.com>
2021-12-07 23:18:53 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const app = express();
+
+const port = 8000;
+
+app.get('/',(req,res)=>{
+    res.send('hello')
+})
+
+app.listen(port,()=>{
+    console.log('port is open at ${port}' )
+})
+
+
+
+
